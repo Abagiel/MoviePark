ad2be124b75fed25a09f4ede761e7c18f31422e3
Abagiel <66873762+Abagiel@users.noreply.github.com>
2020-09-06 15:33:44 +0300

    added auto sliding

--- assets/scripts/toggler.js
+++ assets/scripts/toggler.js
@@ -5,6 +5,7 @@ const togglers = {
 		// То, что будем двигать
 		togglerLine: document.querySelector('.about-toggler'),
 		currentElem: 0,
+		autoIdx: 0,
 		currentOffset: 0,
 		// То, к чему применяем изменение цвета
 		btnsToggler: document.querySelector('[data-elem-about]').querySelectorAll('[data-elem-toggler]')
@@ -13,6 +14,7 @@ const togglers = {
 		containerToggler: document.querySelector('[data-elem-plan]'),
 		togglerLine: document.querySelector('.plan-toggler'),
 		currentElem: 0,
+		autoIdx: 0,
 		currentOffset: 0,
 		btnsToggler: document.querySelector('[data-elem-plan]').querySelectorAll('[data-elem-toggler]')
 	},
@@ -20,10 +22,17 @@ const togglers = {
 		containerToggler: document.querySelector('[data-elem-price]'),
 		togglerLine: document.querySelector('.price-toggler'),
 		currentElem: 0,
+		autoIdx: 0,
 		currentOffset: 0,
 		btnsToggler: document.querySelector('[data-elem-price]').querySelectorAll('[data-elem-toggler]')
 	}
-}
+};
+
+let pageWidth = window.innerWidth;
+let timer = null;
+const time = 2000;
+
+autoChangeElem();
 
 
 // FUNCTIONS
@@ -49,15 +58,62 @@ function changeElem(idx, target) {
 
 		target['togglerLine'].style.transform = `translateX(${target['currentOffset'] - offset}%)`;
 		target['currentOffset'] -= offset;
-		target['currentElem'] = idx;
 	}
 	if (idx < target['currentElem']) {
 		let offset = (target['currentElem'] - idx) * 100;
 
 		target['togglerLine'].style.transform = `translateX(${target['currentOffset'] + offset}%)`;
 		target['currentOffset'] += offset;
-		target['currentElem'] = idx;
 	}
+	
+	target['currentElem'] = idx;
+	target['autoIdx'] = idx;
+}
+
+// Автоматическая смена элементов
+function autoChangeElem() {
+	if (pageWidth < 768) {
+		timer = setInterval(() => {
+
+			changeElem(togglers['plan'].autoIdx, togglers['plan']);
+			changeElem(togglers['about'].autoIdx, togglers['about']);
+			changeElem(togglers['price'].autoIdx, togglers['price']);
+
+			changeAutoIdx();
+			
+		}, time)
+	}
+}
+
+function changeAutoIdx() {
+	const about = togglers['about'];
+	const price = togglers['price'];
+	const plan = togglers['plan'];
+
+	if (about.autoIdx === about.btnsToggler.length - 1) {
+		about.autoIdx = 0;
+	} else {
+		about.autoIdx++;
+	}
+
+	if (price.autoIdx === price.btnsToggler.length - 1) {
+		price.autoIdx = 0;
+	} else {
+		price.autoIdx++;
+	}
+
+	if (plan.autoIdx === plan.btnsToggler.length - 1) {
+		plan.autoIdx = 0;
+	} else {
+		plan.autoIdx++;
+	}
+}
+
+// Обработчик на изменение размера экрана
+function resizeHandler() {
+	pageWidth = window.innerWidth;
+	clearInterval(timer);
+	autoChangeElem();
 }
 
 // EVENT LISTENERS
@@ -66,18 +122,20 @@ const plan = togglers['plan']['containerToggler'];
 const price = togglers['price']['containerToggler'];
 
 about.addEventListener('click', e => {
-	if (e.target.dataset.elemToggler != '') {
+	if (e.target.dataset.elemToggler) {
 		changeElem(e.target.dataset.elemToggler, togglers['about']);
 	}
 });
 plan.addEventListener('click', e => {
-	if (e.target.dataset.elemToggler != '') {
+	if (e.target.dataset.elemToggler) {
 		changeElem(e.target.dataset.elemToggler, togglers['plan']);
 	}
 });
 price.addEventListener('click', e => {
-	if (e.target.dataset.elemToggler != '') {
+	if (e.target.dataset.elemToggler) {
 		changeElem(e.target.dataset.elemToggler, togglers['price'])
 	}
 });
+
+window.onresize = () => resizeHandler();
 
